734cb6f36ed0242dd94ca0b0ec702f9c1e3586d5
Didik Ismawanto <ismawantodidik@gmail.com>
2017-10-03 14:51:45 +0700

    Add runtime permissions for storage #6

--- app/src/main/java/com/naman/dev/androidmakeover/MasterListActivity.java
+++ app/src/main/java/com/naman/dev/androidmakeover/MasterListActivity.java
@@ -1,16 +1,24 @@
 package com.naman.dev.androidmakeover;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.naman.dev.androidmakeover.ui.MasterListFragment;
 
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
 public class MasterListActivity extends AppCompatActivity implements MasterListFragment.OnImageClickListener {
 
+    private static final String TAG = "MasterListActivity";
+    private static final int REQUEST_CODE = 100;
     private int headIndex;
     private int bodyIndex;
     private int legIndex;
@@ -19,6 +27,7 @@ public class MasterListActivity extends AppCompatActivity implements MasterListF
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_master_fragment);
+        requestPermission();
     }
 
     @Override
@@ -57,5 +66,28 @@ public class MasterListActivity extends AppCompatActivity implements MasterListF
                 startActivity(i);
             }
         });
+    }
+
+    @AfterPermissionGranted(REQUEST_CODE)
+    private void requestPermission() {
+        String[] perms = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        if (EasyPermissions.hasPermissions(this, perms)) {
+            // Already have permission, do the thing
+            // ...
+            Log.d(TAG, "permission granted");
+        } else {
+            // Do not have permissions, request them now
+            EasyPermissions.requestPermissions(this, getString(R.string.rationale_external),
+                    REQUEST_CODE, perms);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // Forward results to EasyPermissions
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
     }
 }
